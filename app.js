26cb0c787373604c56244ba2afdad67172a467a4
hryJouh26 <amancio26jouh@gmail.com>
2020-11-23 13:59:27 +0800

    Change Directories

--- app.js
+++ app.js
@@ -31,75 +31,20 @@ database.connect()
 
 // ---- Define all application routes ---- //
 
-
-// --- Directing Home Page --- //
-app.get('/', (req, res) => {
-    res.render('pages/index', {
-        title: 'HighQua HomePage'
-    })
-})
-
 const authRoutes = require('./modules/auth/routes/AuthRoutes')
 const adminPagesRoutes = require('./routes/AdminPagesRoutes')
 const accountRoutes = require('./modules/account/routes/AccountRoutes')
 const roomRoutes = require('./routes/RoomsRoute')
 const reportRoutes = require('./routes/ReportsRoute')
 
-app.get('/', (req, res) => {
-    res.render('pages/index')
-})
-
-// app.get('/', (req, res) => {
-//     res.render('pages/rooms/roomlist', {
-//         title: 'HighQua || Home page'
-//     })
-// });
-
-// --- Directing Room List Page --- //
-app.get('/rooms', (req, res) => {
-    res.render('pages/rooms/roomlist', {
-        title: 'HighQua Room Lists'
-    })
-});
-
-// --- Directing Blog Page --- //
-app.get('/blog', (req, res) => {
-    res.render('pages/blog', {
-        title: 'HighQua Blog'
-    })
-});
-
-// --- Directing Contact Page --- //
-app.get('/contact', (req, res) => {
-    res.render('pages/contact', {
-        title: 'HighQua Contact'
-    })
-});
-app.get('/test', (req, res) => {
-    res.render('pages/test', {
-        layout: ''
-    })
-});
-
-app.get('/services', (req, res) => {
-    res.render('pages/services', {
-        title: 'HighQua Services'
-    })
-});
-
-// --- Directing Login and Register Page --- //
-app.get('/login', (req, res) => {
-    res.render('auth/login', {
-        title: 'Login'
-    })
-});
 
 
-
+app.use('/', require('./routes/ClientPagesRoutes'))
 app.use('/auth', authRoutes)
 app.use('/admin', adminPagesRoutes)
 app.use('/accounts', accountRoutes)
 app.use('/reports', reportRoutes)
 app.use('/rooms', roomRoutes)
+
 
 app.listen(port, console.log(`Application is running at port ${port}`))
